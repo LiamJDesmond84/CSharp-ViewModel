22481356497d6ce2f4d01a04fe35b7216201ae77
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-26 20:22:22 -0500

    Updating controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,29 @@ namespace CSharp_ViewModel.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
     public IActionResult Names()
     {
         // to a View that has defined a model as @model string[]
--- Models/User.cs
+++ Models/User.cs
@@ -6,4 +6,4 @@
         public string LastName { get; set; }
     }
 }
-}
+
