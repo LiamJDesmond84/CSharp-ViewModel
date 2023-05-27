b09eaa326f75d56010dc022da5917b0a2dcb3c38
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-26 20:17:33 -0500

    Adding other stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,16 @@ namespace CSharp_ViewModel.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public IActionResult Names()
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // to a View that has defined a model as @model string[]
+        string[] names = new string[]
+        {
+            "Sally",
+            "Billy",
+            "Joey",
+            "Moose"
+        };
+        return View(names);
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -2,5 +2,8 @@
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
+}
 }
